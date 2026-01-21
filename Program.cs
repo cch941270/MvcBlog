0a5f32763bcd63e4a860bfe90b88db6bf22d4143
cch94 <cch1270@tutamail.com>
2026-01-22 01:35:11 +0800

    Set default controller to Blogs

--- Program.cs
+++ Program.cs
@@ -37,7 +37,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Blogs}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
